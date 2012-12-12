3701b5f6aa6d0e283ec1f63b868a7978e57e1ba1
William Hua <william.hua@canonical.com>
2012-12-12 09:21:29 -0500

    Remember to support accels.

--- src/unity-gtk-menu-section.c
+++ src/unity-gtk-menu-section.c
@@ -1,5 +1,9 @@
 #include "unity-gtk-menu-section.h"
 
+#ifndef G_MENU_ATTRIBUTE_ACCEL
+#define G_MENU_ATTRIBUTE_ACCEL "accel"
+#endif
+
 G_DEFINE_TYPE (UnityGtkMenuSection,
                unity_gtk_menu_section,
                G_TYPE_MENU_MODEL);
@@ -199,6 +203,25 @@ unity_gtk_menu_section_get_item_attributes (GMenuModel  *model,
         g_hash_table_insert (*attributes, G_MENU_ATTRIBUTE_TARGET, g_variant_ref_sink (g_variant_new_string (action->name)));
 
       g_free (name);
+    }
+
+  if (item->menu_item != NULL)
+    {
+      const gchar *accel_path = gtk_menu_item_get_accel_path (item->menu_item);
+
+      if (accel_path != NULL)
+        {
+          GtkAccelKey accel_key;
+
+          if (gtk_accel_map_lookup_entry (accel_path, &accel_key))
+            {
+              gchar *accel_name = gtk_accelerator_name (accel_key.accel_key, accel_key.accel_mods);
+
+              g_hash_table_insert (*attributes, G_MENU_ATTRIBUTE_ACCEL, g_variant_ref_sink (g_variant_new_string (accel_name)));
+
+              g_free (accel_name);
+            }
+        }
     }
 }
 
