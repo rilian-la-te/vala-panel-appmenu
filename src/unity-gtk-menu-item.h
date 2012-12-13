d0b25ee631105444cd8cea705d7484e7fc34857a
William Hua <william.hua@canonical.com>
2012-12-13 14:11:02 -0500

    Copy notices.

--- src/unity-gtk-menu-item.h
+++ src/unity-gtk-menu-item.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2012 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Ryan Lortie <desrt@desrt.ca>
+ *          William Hua <william.hua@canonical.com>
+ */
+
 #ifndef __UNITY_GTK_MENU_ITEM_H__
 #define __UNITY_GTK_MENU_ITEM_H__
 
