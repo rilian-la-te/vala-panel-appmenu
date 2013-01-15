e6c65ec9b84eb8c1910941b62241c2cc9a1b5b2a
William Hua <william.hua@canonical.com>
2013-01-15 15:23:43 -0500

    Remove debian/*.ex.

--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libunity-gtk-module 0.0.0 unity-gtk-module (>> 0.0.0-0), unity-gtk-module (<< 0.0.0-99)
--- debian/unity-gtk-module.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the unity-gtk-module package
-#
-0 4	* * *	root	[ -x /usr/bin/unity-gtk-module_maintenance ] && /usr/bin/unity-gtk-module_maintenance
