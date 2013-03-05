44e0dd5d6f3306f244030bed8c9f8e0963e31681
William Hua <william.hua@canonical.com>
2013-03-05 17:49:31 -0500

    Make the tests work properly.

--- tests/autopilot/tests/test_gedit.py
+++ tests/autopilot/tests/test_gedit.py
@@ -20,62 +20,71 @@ class GeditTestCase(unity.tests.UnityTestCase, autopilot.introspection.gtk.GtkIn
         registry = pyatspi.registry.Registry()
         self.desktop = registry.getDesktop(0)
 
-        time.sleep(0.2)
+        time.sleep(2.0)
 
     def test_file_new(self):
         """Test if menu item insertion works."""
+        panel = self.unity.panels.get_active_panel()
+        time.sleep(2.0)
+        menu = panel.menus.get_menu_by_label('_Documents')
+        time.sleep(2.0)
+        menu.mouse_click()
+        time.sleep(2.0)
+        menu.mouse_click()
+        time.sleep(2.0)
+
         panel = pyatspi.utils.findAllDescendants(self.desktop, lambda a: a.name == 'unity-panel-service')[0]
         menubar = panel[0]
-        documents_item = menubar[7]
+        documents_item = menubar[5]
         documents_menu = documents_item[0]
-
-        panel = self.unity.panels.get_active_panel()
-        time.sleep(0.2)
-        menu = panel.menus.get_menu_by_label('_File')
-        time.sleep(0.2)
-        menu.mouse_click()
-        time.sleep(0.2)
-
-        untitled_document_1_item = documents_menu[-3]
+        untitled_document_1_item = documents_menu[-1]
         self.assertTrue(untitled_document_1_item.name == 'Untitled Document 1')
         self.assertTrue(untitled_document_1_item.get_state_set().contains(pyatspi.STATE_CHECKED))
 
+        panel = self.unity.panels.get_active_panel()
+        time.sleep(2.0)
+        menu = panel.menus.get_menu_by_label('_File')
+        time.sleep(2.0)
+        menu.mouse_click()
+        time.sleep(2.0)
+
         self.keyboard.press_and_release('Down')
-        time.sleep(0.2)
+        time.sleep(2.0)
         self.keyboard.press_and_release('Enter')
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         menu = panel.menus.get_menu_by_label('_Documents')
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         tabs = self.app.select_many('GeditTab')
         self.assertTrue(len(tabs) == 2)
         self.assertTrue(tabs[0].name == 'Untitled Document 1')
         self.assertTrue(tabs[1].name == 'Untitled Document 2')
 
-        untitled_document_2_item = documents_menu[-3]
+        untitled_document_1_item = documents_menu[-2]
+        untitled_document_2_item = documents_menu[-1]
         self.assertTrue(untitled_document_1_item.name == 'Untitled Document 1')
-        # self.assertTrue(untitled_document_2_item.name == 'Untitled Document 2')
-        # self.assertFalse(untitled_document_1_item.get_state_set().contains(pyatspi.STATE_CHECKED))
-        # self.assertTrue(untitled_document_2_item.get_state_set().contains(pyatspi.STATE_CHECKED))
+        self.assertTrue(untitled_document_2_item.name == 'Untitled Document 2')
+        self.assertFalse(untitled_document_1_item.get_state_set().contains(pyatspi.STATE_CHECKED))
+        self.assertTrue(untitled_document_2_item.get_state_set().contains(pyatspi.STATE_CHECKED))
 
     def test_file_quit(self):
         """Test if menu item activation works."""
         panel = self.unity.panels.get_active_panel()
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu = panel.menus.get_menu_by_label('_File')
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         self.keyboard.press_and_release('Up')
-        time.sleep(0.2)
+        time.sleep(2.0)
         self.keyboard.press_and_release('Enter')
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         self.assertFalse(self.app_is_running('Text Editor'))
 
@@ -85,7 +94,7 @@ class GeditTestCase(unity.tests.UnityTestCase, autopilot.introspection.gtk.GtkIn
         menubar = panel[0]
         view_item = menubar[2]
         view_menu = view_item[0]
-        toolbar_item = view_menu[1]
+        toolbar_item = view_menu[0]
         checked = toolbar_item.get_state_set().contains(pyatspi.STATE_CHECKED)
 
         toolbar = self.app.select_many('GtkToolbar')[0]
@@ -94,31 +103,36 @@ class GeditTestCase(unity.tests.UnityTestCase, autopilot.introspection.gtk.GtkIn
         self.assertTrue(checked == visible)
 
         panel = self.unity.panels.get_active_panel()
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu = panel.menus.get_menu_by_label('_View')
-        time.sleep(0.2)
+        time.sleep(2.0)
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         self.keyboard.press_and_release('Down')
-        time.sleep(0.2)
+        time.sleep(2.0)
         self.keyboard.press_and_release('Enter')
-        time.sleep(0.2)
+        time.sleep(2.0)
+
+        menu.mouse_click()
+        time.sleep(2.0)
+        menu.mouse_click()
+        time.sleep(2.0)
 
         self.assertTrue(checked == visible)
         self.assertFalse(toolbar.visible == visible)
         self.assertFalse(toolbar_item.get_state_set().contains(pyatspi.STATE_CHECKED) == checked)
 
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         self.keyboard.press_and_release('Down')
-        time.sleep(0.2)
+        time.sleep(2.0)
         self.keyboard.press_and_release('Enter')
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         menu.mouse_click()
-        time.sleep(0.2)
+        time.sleep(2.0)
 
         self.assertTrue(checked == visible)
         self.assertTrue(toolbar.visible == visible)
