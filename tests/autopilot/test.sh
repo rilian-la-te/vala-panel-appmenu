26896db3d01fbb4a729819f00d597dcac622af98
William Hua <william.hua@canonical.com>
2013-03-07 16:00:58 -0500

    Fix make test target, read tests from list.

--- tests/autopilot/test.sh
+++ tests/autopilot/test.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-autopilot run tests.test_gedit.GeditTestCase.test_file_new
-autopilot run tests.test_gedit.GeditTestCase.test_file_close
-autopilot run tests.test_gedit.GeditTestCase.test_file_quit
-autopilot run tests.test_gedit.GeditTestCase.test_edit_undo
-autopilot run tests.test_gedit.GeditTestCase.test_view_toolbar
-autopilot run tests.test_gedit.GeditTestCase.test_documents_untitled_document
-autopilot run tests.test_gedit.GeditTestCase.test_ctrl_n
+for name in `autopilot list tests | head -n -3 | tail -n +3`
+do
+  autopilot run $name || exit 1
+done
