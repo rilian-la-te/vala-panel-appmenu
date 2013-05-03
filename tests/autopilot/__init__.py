11503ba332f1322e5e2b8c415697d7194d7b5bdd
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2013-05-03 02:21:12 +0200

    Create a seperate autopilot package, since we don't run integration tests during build

---
+++ tests/autopilot/__init__.py
@@ -0,0 +1,6 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+# Copyright 2013 Canonical
+#
+# This file is part of unity-gtk-module.
+
+"""unity-gtk-module autopilot tests - top level package."""
