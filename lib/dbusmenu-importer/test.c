685d39c793f63936b14c2579ca1af2d973af5e9f
Konstantin <ria.freelander@gmail.com>
2018-01-31 18:23:54 +0300

    Importer: add license header to all files.

--- lib/dbusmenu-importer/test.c
+++ lib/dbusmenu-importer/test.c
@@ -1,3 +1,21 @@
+/*
+ * vala-panel-appmenu
+ * Copyright (C) 2018 Konstantin Pugin <ria.freelander@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "debug.h"
 #include "importer.h"
 #include <gtk/gtk.h>
@@ -38,13 +56,15 @@ int main(int argc, char *argv[])
 
 	GtkMenuBar *menubar = gtk_menu_bar_new();
 	GtkMenuButton *menu = gtk_menu_button_new();
-	//            DBusMenuImporter *importer = dbus_menu_importer_new("org.krusader",
-	//            "/MenuBar/2");
-	//	DBusMenuImporter *importer = dbus_menu_importer_new(":1.46", "/MenuBar/2");
-	DBusMenuImporter *importer =
-	    dbus_menu_importer_new(":1.227", "/com/canonical/menu/300003e");
-	//    DBusMenuImporter *importer =
-	//        dbus_menu_importer_new(":1.198", "/com/canonical/menu/5A00001");
+	//	DBusMenuImporter *importer = dbus_menu_importer_new("org.krusader", "/MenuBar/2");
+	DBusMenuImporter *importer = dbus_menu_importer_new(":1.250", "/MenuBar/1");
+	//	DBusMenuImporter *importer = dbus_menu_importer_new(":1.49", "/MenuBar/2");
+	//	DBusMenuImporter *importer =
+	//	    dbus_menu_importer_new(":1.227", "/com/canonical/menu/300003e");
+	//	DBusMenuImporter *importer =
+	//	    dbus_menu_importer_new(":1.458", "/com/canonical/menu/4600016");
+	//	DBusMenuImporter *importer =
+	//	    dbus_menu_importer_new(":1.542", "/com/canonical/menu/2600041");
 	g_signal_connect(importer, "notify::model", G_CALLBACK(on_importer_model_changed), menubar);
 	g_signal_connect(importer, "notify::model", G_CALLBACK(on_importer_model_changed), menu);
 	g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
