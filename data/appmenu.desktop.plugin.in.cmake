b1a0c28656c09c3de7af79d861df5197c95c22d0
Konstantin <ria.freelander@gmail.com>
2018-08-19 00:22:54 +0300

    data: use confgure_file.

---
+++ data/appmenu.desktop.plugin.in.cmake
@@ -0,0 +1,11 @@
+[Plugin]
+Module=appmenu
+Name=AppMenu Plugin
+Description=Display AppMenu (Global Menu)
+Icon=preferences-system-symbolic
+Website=https://gitlab.com/vala-panel-project/vala-panel-appmenu
+HelpURI=https://gitlab.com/vala-panel-project/vala-panel-appmenu
+License=lgpl
+X-ValaPanel-Expandable=true
+Expandable=true
+Version=@VERSION@
