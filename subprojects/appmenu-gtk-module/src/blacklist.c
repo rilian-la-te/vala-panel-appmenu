edac736dd29f2f1cd5f287d256ac4794dd51c5e6
Konstantin <ria.freelander@gmail.com>
2019-07-16 18:12:33 +0300

    appmenu-gtk-module: fix crashes on blacklist

--- subprojects/appmenu-gtk-module/src/blacklist.c
+++ subprojects/appmenu-gtk-module/src/blacklist.c
@@ -58,10 +58,7 @@ static bool is_string_in_array(const char *string, GVariant *array)
 	while (g_variant_iter_loop(&iter, "&s", &element))
 	{
 		if (g_strcmp0(element, string) == 0)
-		{
-			g_clear_pointer(&element, g_free);
 			return true;
-		}
 	}
 
 	return false;
